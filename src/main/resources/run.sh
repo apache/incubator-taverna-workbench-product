26f9aa6c34c98ba9d3917ddb31e35d3fa2f76b23
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-05-28 11:53:18 +0000

    Updated to cope with links to run.sh
    git-svn-id: https://taverna.googlecode.com/svn/taverna/products/net.sf.taverna.t2.taverna-workbench/trunk/workbench-distro@7739 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/resources/run.sh
+++ src/main/resources/run.sh
@@ -1,7 +1,29 @@
-#!/bin/sh
+ #!/bin/sh
+
+## resolve links - $0 may be a symlink
+PRG="$0"
+progname=`basename "$0"`
+saveddir=`pwd`
+
+# need this for relative symlinks
+cd "$(dirname "$PRG")"
+while [ -h "$PRG" ] ; do
+    ls=$(ls -ld "$PRG")
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '.*/.*' > /dev/null; then
+	PRG="$link"
+    else
+	PRG=`dirname "$PRG"`"/$link"
+    fi
+done
+  
+TAVERNA_HOME="`dirname "$PRG"`"
+cd $TAVERNA_HOME
+
 exec java -Xmx300m -Draven.profile=file:conf/current-profile.xml \
   -Dtaverna.startup=. \
   -Djava.system.class.loader=net.sf.taverna.raven.prelauncher.BootstrapClassLoader \
-  -Dapple.laf.useScreenMenuBar=true \
+  -Dapple.laf.useScreenMenuBar=false \
   -Dsun.swing.enableImprovedDragGesture \
   -jar lib/prelauncher-*.jar
+  
