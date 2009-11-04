e7071ad9d6f482cf40dfb28282eff71d616990be
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-11-04 11:13:31 +0000

    comments on memory
    git-svn-id: https://taverna.googlecode.com/svn/taverna/products/net.sf.taverna.t2.taverna-workbench/trunk/workbench-distro@8839 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/resources/run.bat
+++ src/main/resources/run.bat
@@ -6,15 +6,14 @@ REM go to the distribution directory
 pushd "%~dp0"
 
 
-REM 300 MB memory
+REM 300 MB memory, 140 MB for classes
 set ARGS=-Xmx300m -XX:MaxPermSize=140m
 
 REM Internal system properties
 set ARGS=%ARGS% -Draven.profile=file:conf/current-profile.xml
 set ARGS=%ARGS% -Djava.system.class.loader=net.sf.taverna.raven.prelauncher.BootstrapClassLoader 
 set ARGS=%ARGS% -Dsun.swing.enableImprovedDragGesture 
-REM set ARGS=%ARGS% "-Dtaverna.startup=%~dp0"
-set ARGS=%ARGS% "-Dtaverna.startup=."
+set ARGS=%ARGS% -Dtaverna.startup=.
 
 java %ARGS% -jar lib\prelauncher-*.jar
 
