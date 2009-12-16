e1b22e4f4520a543dc4ef072edb5df8dce64d600
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-12-16 15:27:09 +0000

    T2-936 readme files etc
    git-svn-id: https://taverna.googlecode.com/svn/taverna/products/net.sf.taverna.t2.taverna-workbench/tags/taverna-workbench-2.1.0/workbench-distro@9598 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ src/main/resources/taverna.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+## resolve links - $0 may be a symlink
+PRG="$0"
+progname=`basename "$0"`
+saveddir=`pwd`
+
+# need this to resolve relative symlinks
+cd "`dirname "$PRG"`"
+while [ -h "$PRG" ] ; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '.*/.*' > /dev/null; then
+        PRG="$link"
+    else
+        PRG=`dirname "$PRG"`"/$link"
+    fi
+done
+
+TAVERNA_HOME="`dirname "$PRG"`"
+cd $TAVERNA_HOME
+
+# 300 MB memory, 140 MB for classes
+exec java -Xmx300m -XX:MaxPermSize=140m \
+  -Draven.profile=file:conf/current-profile.xml \
+  -Dtaverna.startup=. \
+  -Djava.system.class.loader=net.sf.taverna.raven.prelauncher.BootstrapClassLoader \
+  -Dapple.laf.useScreenMenuBar=true \
+  -Dapple.awt.graphics.UseQuartz=false \
+  -Dsun.swing.enableImprovedDragGesture \
+  -jar lib/prelauncher-*.jar
