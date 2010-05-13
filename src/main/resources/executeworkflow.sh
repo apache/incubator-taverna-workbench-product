ee7023152dd77c4b58ec395fcce99afff3305b06
stuart@mygrid.org.uk <stuart@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-05-13 16:44:37 +0000

    added stuff for running workflows from the commandline (*Nix only so far)
    git-svn-id: https://taverna.googlecode.com/svn/taverna/products/net.sf.taverna.t2.taverna-workbench/trunk/workbench-distro@10182 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ src/main/resources/executeworkflow.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+## resolve links - $0 may be a symlink
+PRG="$0"
+progname=`basename "$0"`
+saveddir=`pwd`
+
+# need this to resolve relative symlinks
+cd "`dirname "$PRG"`"
+while [ -h "$PRG" ] ; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '.*/.*' > /dev/null; then
+        PRG="$link"
+    else
+        PRG=`dirname "$PRG"`"/$link"
+    fi
+done
+
+TAVERNA_HOME="`dirname "$PRG"`"
+cd $TAVERNA_HOME
+
+# 300 MB memory, 140 MB for classes
+exec java -Xmx300m -XX:MaxPermSize=140m \
+  -Draven.profile=file:conf/current-profile.xml \
+  -Dtaverna.startup=. \
+  -Djava.system.class.loader=net.sf.taverna.raven.prelauncher.BootstrapClassLoader \
+  -Dapple.laf.useScreenMenuBar=true \
+  -Dapple.awt.graphics.UseQuartz=false \
+  -Dsun.swing.enableImprovedDragGesture \
+  -Draven.launcher.app.main=net.sf.taverna.t2.commandline.CommandLineLauncher \
+  -Draven.launcher.show_splashscreen=false \
+  -jar lib/prelauncher-*.jar \
+  $@
