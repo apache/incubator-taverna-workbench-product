e989a9e06f67a3027ecd0e592a3838e9070e732a
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-06-10 09:42:39 +0000

    T2-1348 make shellscript work with symlinks properly
    git-svn-id: https://taverna.googlecode.com/svn/taverna/products/net.sf.taverna.t2.taverna-workbench/trunk/workbench-distro@10805 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/resources/taverna.sh
+++ src/main/resources/taverna.sh
@@ -1,28 +1,18 @@
 #!/bin/sh
 
 ## resolve links - $0 may be a symlink
-PRG="$0"
-progname=`basename "$0"`
-saveddir=`pwd`
-
-# need this to resolve relative symlinks
-cd "`dirname "$PRG"`"
-while [ -h "$PRG" ] ; do
-    ls=`ls -ld "$PRG"`
-    link=`expr "$ls" : '.*-> \(.*\)$'`
-    if expr "$link" : '.*/.*' > /dev/null; then
-        PRG="$link"
-    else
-        PRG=`dirname "$PRG"`"/$link"
-    fi
-done
+prog="$0"
+realprog=`readlink -e "$prog"`
+taverna_home=`dirname "$realprog"`
 
 # 400 MB memory, 140 MB for classes
 exec java -Xmx400m -XX:MaxPermSize=140m \
-  -Draven.profile=file:conf/current-profile.xml \
-  -Dtaverna.startup=. \
+  "-Draven.profile=file://$taverna_home/conf/current-profile.xml" \
+  "-Dtaverna.startup=$taverna_home" \
   -Djava.system.class.loader=net.sf.taverna.raven.prelauncher.BootstrapClassLoader \
   -Dapple.laf.useScreenMenuBar=true \
   -Dapple.awt.graphics.UseQuartz=false \
   -Dsun.swing.enableImprovedDragGesture \
-  -jar lib/prelauncher-*.jar
+  -jar "$taverna_home/lib/"prelauncher-*.jar \
+  $@
+  
