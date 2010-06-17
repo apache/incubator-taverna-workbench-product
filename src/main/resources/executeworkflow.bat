e4c9c67d22754152473fa129ec4b73fe28341e0a
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-06-17 15:20:55 +0000

    T2-1400 resolve relative paths to taverna-debug.bat and executeworkflow.bat on Windows
    git-svn-id: https://taverna.googlecode.com/svn/taverna/products/net.sf.taverna.t2.taverna-workbench/trunk/workbench-distro@10952 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/resources/executeworkflow.bat
+++ src/main/resources/executeworkflow.bat
@@ -2,22 +2,20 @@
 
 REM Taverna startup script
 
-REM go to the distribution directory
-pushd "%~dp0"
+REM distribution directory
+set TAVERNA_HOME=%~dp0
 
 
-REM 400 MB memory, 140 MB for classes
-set ARGS=-Xmx400m -XX:MaxPermSize=140m
+REM 300 MB memory, 140 MB for classes
+set ARGS=-Xmx300m -XX:MaxPermSize=140m
 
-REM Internal system properties
-set ARGS=%ARGS% -Draven.profile=file:conf/current-profile.xml
+REM Taverna system properties
+set ARGS=%ARGS% "-Draven.profile=file:%TAVERNA_HOME%conf/current-profile.xml"
 set ARGS=%ARGS% -Djava.system.class.loader=net.sf.taverna.raven.prelauncher.BootstrapClassLoader 
 set ARGS=%ARGS% -Draven.launcher.app.main=net.sf.taverna.t2.commandline.CommandLineLauncher
 set ARGS=%ARGS% -Draven.launcher.show_splashscreen=false
-set ARGS=%ARGS% -Dtaverna.startup=.
+set ARGS=%ARGS% "-Dtaverna.startup=%TAVERNA_HOME%."
 
-java %ARGS% -jar lib\prelauncher-*.jar %*
+java %ARGS% -jar "%TAVERNA_HOME%lib\prelauncher-2.3-SNAPSHOT.jar" %*
 
-pause
-REM restore current directory
-popd
+
