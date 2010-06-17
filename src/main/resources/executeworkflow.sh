9aeb5b3f6f72bb91b19541b08582b158db80119b
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-06-17 13:32:03 +0000

    T2-1406 update shell script to work on OSX
    git-svn-id: https://taverna.googlecode.com/svn/taverna/products/net.sf.taverna.t2.taverna-workbench/trunk/workbench-distro@10951 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/resources/executeworkflow.sh
+++ src/main/resources/executeworkflow.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+set -e
+
 ## resolve links - $0 may be a symlink
 prog="$0"
-realprog=`readlink -e "$prog"`
+
+real_path() {
+    readlink -m "$1" 2>/dev/null || python -c 'import os,sys;print os.path.realpath(sys.argv[1])' "$1"
+}
+
+realprog=`real_path "$prog"`
 taverna_home=`dirname "$realprog"`
 
 # 300 MB memory, 140 MB for classes
