b6bdad07203fb2016dcea200bb3d5d46068260fd
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2014-01-17 10:55:37 +0000

    taverna.dotlocation
    git-svn-id: https://taverna.googlecode.com/svn/taverna/products/net.sf.taverna.t2.taverna-workbench/branches/maintenance/workbench-distro@16532 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/resources/taverna.sh
+++ src/main/resources/taverna.sh
@@ -16,15 +16,17 @@ if test -x "$JAVA_HOME/bin/java"; then
     javabin="$JAVA_HOME/bin/java"
 fi
 
-# 1000 MB memory, 140 MB for classes
-exec "$javabin" -Xmx1000m -XX:MaxPermSize=140m \
-  "-Dcom.sun.net.ssl.enableECC=false" \
-  "-Djsse.enableSNIExtension=false" \
+# 32-bit compatible memory settings:
+# 700 MB memory, 140 MB for classes
+exec "$javabin" -Xmx700m -XX:MaxPermSize=140m \
   "-Draven.profile=file://$taverna_home/conf/current-profile.xml" \
   "-Dtaverna.startup=$taverna_home" \
+  "-Dtaverna.dotlocation=$taverna_home/bin/dot.sh"
   -Djava.system.class.loader=net.sf.taverna.raven.prelauncher.BootstrapClassLoader \
   -Dapple.laf.useScreenMenuBar=true \
   -Dapple.awt.graphics.UseQuartz=false \
+  -Dcom.sun.net.ssl.enableECC=false \
+  -Djsse.enableSNIExtension=false \
   -Dsun.swing.enableImprovedDragGesture \
   -jar "$taverna_home/lib/"prelauncher-*.jar \
   ${1+"$@"}
